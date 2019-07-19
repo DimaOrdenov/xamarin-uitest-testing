13d884ddf33b19307af210043662221f7d3c1255
dmitry.ordenov <dmitry.ordenov@anywayanyday.com>
2019-07-19 18:10:15 +0300

    Added some tests logic

---
+++ UITestProject/AppInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITestProject
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                //System.IO.Directory.SetCurrentDirectory(@"C:\Users\dmitry.ordenov");
+
+                return ConfigureApp
+                    .Android
+                    .InstalledApp("com.dmitryordenov.TestApplication")
+                    .Debug()
+                    .EnableLocalScreenshots()
+                    .DeviceSerial("d999fec8")
+                    .StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
